2cf746eb50a2f4c93ecaaddc0d7f25df8fedd233
Johnny Casares <88862676+JohnnyCasares@users.noreply.github.com>
2022-11-21 01:01:29 +0000

    fixed 'n' row hiragana symbols

--- .ccls-cache/@home@runner@eel-Final-Project/main.c
+++ .ccls-cache/@home@runner@eel-Final-Project/main.c
@@ -1,19 +1,23 @@
 #include "functions.h"
-#include <locale.h>
+// #include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
 char word[]; // initialize extern var
+char romaji_to_hiragana(char word[]);
 int main() {
 
-  setlocale(LC_ALL, "ja_JP.UTF8");
+  // setlocale(LC_ALL, "ja_JP.UTF8");
   banner(); // Print banner
   // word look up
   printf("\n\nWelcome back\n\a");
   printf("Look up a word (enter it in romaji): ");
-  scanf("%s", word);
-  // print user's word, hiragana version, and katakana
-  printf("\nyour word is %s", word);
+  scanf("%[^\n]", word);
+  // print user's word and hiragana version
+  // romaji_to_hiragana(word);
+  printf("\nyour word is %s\t", word);
 
+  romaji_to_hiragana(word);
+  
 
     
   return 0;
